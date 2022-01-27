89d0fb6fd7f28e05f2e9d0ad08cc2f8633b55239
Hector Nava <hector.r.nava44@gmail.com>
2022-01-27 15:20:04 -0700

    Added button event to post task

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,42 @@
 $(document).ready(async function() {
     const $todos = $('.todos');
+    const $addTodoBtn = $('#addTodo');
+    const $taskInput = $('#task');
+    const $userIdInput = $('#userId');
+
+    $addTodoBtn.on('click', async function(event) {
+        event.preventDefault();
+        const newTodo = {
+            task: $taskInput.val(),
+            userId: $userIdInput.val()
+        };
+
+        try {
+            const createdTodo = await $.post('/api/todos', newTodo);
+            const $ul = $('<ul>').addClass('list-group list-group-horizontal');
+            const $taskLi = $('<li>').text(createdTodo.task).addClass('list-group-item');
+            const $usernameLi = $('<li>').text(createdTodo.username).addClass('list-group-item');
+            const $btnLi = $('<li>').addClass('list-group-item');
+            const $deleteBtn = $('<button>').text('Delete');
+
+            if (createdTodo.completed) {
+                $taskLi.addClass('list-group-item-success');
+                $usernameLi.addClass('list-group-item-success');
+                $btnLi.addClass('list-group-item-success');
+            } else {
+                $taskLi.addClass('list-group-item-danger');
+                $usernameLi.addClass('list-group-item-danger');
+                $btnLi.addClass('list-group-item-danger');
+            }
+
+            $btnLi.append($deleteBtn);
+            $ul.append($taskLi, $usernameLi, $btnLi);
+            $todos.append($ul);
+        } catch (e) {
+            alert(e);
+        }
+    })
+
     try {
         const todos = await $.get('/api/todos');
         todos.forEach(todo => {
